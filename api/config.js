343e33b217b67635fc12f037385dc8ff31c28700
DragQuaza <maitydebanjan2024@gmail.com>
2026-04-16 20:39:11 +0530

    Fix Vercel deployment: Add serverless api/config.js for dynamic Firebase config, update rewrites

---
+++ api/config.js
@@ -0,0 +1,24 @@
+export default async function handler(req, res) {
+  if (req.method !== 'GET') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
+
+  const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  };
+
+  const backendUrl = process.env.BACKEND_URL || '';
+
+  res
+    .set('Cache-Control', 'no-store')
+    .type('application/javascript')
+    .send(
+      `window.__FIREBASE_CONFIG__ = ${JSON.stringify(firebaseConfig)};\nwindow.BACKEND_URL = ${JSON.stringify(backendUrl)};`
+    );
+}
